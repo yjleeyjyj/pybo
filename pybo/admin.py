22b570748947861ad256b81935dd4462cc00039a
Lee Young June <yjleeyjyj@gmail.com>
2021-08-15 15:46:26 +0900

    RE 파이보 프로젝트 최초 커밋

---
+++ pybo/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+admin.site.register(Question, QuestionAdmin)
---
+++ pybo/templatetags/pybo_filter.py
@@ -0,0 +1,14 @@
+import markdown
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+def sub(value, avg):
+    return value - avg
+
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
